86c1ef7c433fe80390d254ec4895493a49bcbd14
khalique-hamza <software.eng.khalique@gmail.com>
2026-01-01 22:59:06 -0500

    revised test_function.py; first unique commit on test_branch

--- test_function.py
+++ test_function.py
@@ -1,4 +1,7 @@
 from enum_direction import Direction
 
-def test_direction():
+def test_direction_up():
     assert Direction.UP == "up", "Up direction not equal"
+
+def test_direction_down():
+    assert Direction.DOWN == "down", "Down direction not equal"
